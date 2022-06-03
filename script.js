5215e52518071201f383df5b352f7ab17ba0c661
yamanerkam <88717125+yamanerkam@users.noreply.github.com>
2022-06-04 01:10:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+const minuteInput = document.querySelector("#minuteInput")
+const secondInput = document.querySelector("#secondInput")
+const start = document.querySelector("#start")
+const reset = document.querySelector("#reset")
+const timer = document.querySelector("#timer")
+
+
+
+
+
+
+start.addEventListener("click",(e)=>{
+    let control=true;
+    start.disabled=true
+    minuteInput.disabled=true
+    secondInput.disabled=true
+    var minuteInput2 = minuteInput.value;
+    var secondInput2=secondInput.value
+    if(secondInput2==0 && minuteInput2==0){
+        control=false
+     }
+
+var myTimer =setInterval(function() {
+    if(secondInput2==0 && minuteInput2==0){
+       if(control==false){
+        console.log("0000")
+        clearInterval(myTimer)
+        timer.textContent = `Timer=> Give me anything but 0`;
+       }else{
+        console.log("done")
+        clearInterval(myTimer)
+        timer.textContent = `Timer=> done`;
+       }
+
+
+    }
+    else if(secondInput2==0){
+        timer.textContent = `Timer=>  ${minuteInput2} : ${secondInput2} `;
+        minuteInput2--;
+        secondInput2=59
+        console.log("1")
+        e.stopPropagation()
+
+    }
+   else{
+        timer.textContent = `Timer=>  ${minuteInput2} : ${secondInput2--} `;
+        console.log("3")
+    }
+
+    
+    
+    }, 1000);
+
+
+    
+    reset.addEventListener("click",()=>{
+        clearInterval(myTimer)
+        start.disabled=false
+        minuteInput.disabled=false
+    secondInput.disabled=false
+        minuteInput.value=0
+        secondInput.value=0
+        timer.textContent = `Timer=>`;
+    })
+   
+   
+
+})
+
+
